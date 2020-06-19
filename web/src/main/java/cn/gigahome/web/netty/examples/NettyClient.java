ab9f1790a5977a6598ef76a543a9cf3c35137b6e
null <aibw@kaisagroup.com>
2020-06-19 16:34:33 +0800

    消息格式及是否使用SSL各种组合

--- web/src/main/java/cn/gigahome/web/netty/examples/NettyClient.java
+++ web/src/main/java/cn/gigahome/web/netty/examples/NettyClient.java
@@ -1,13 +1,16 @@
 package cn.gigahome.web.netty.examples;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.bytes.ByteArrayEncoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
-import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
@@ -15,54 +18,100 @@ import java.util.UUID;
 
 public class NettyClient {
     public static void main(String[] args) throws InterruptedException {
-        Bootstrap bootstrap = new Bootstrap();
-        NioEventLoopGroup group = new NioEventLoopGroup();
-        MessageDecorator firstDecorator = new MessageDecorator();
-        firstDecorator.setDecorateChar("#");
-        MessageDecorator lastDecorator = new MessageDecorator();
-        lastDecorator.setDecorateChar("*");
-        bootstrap.group(group)
-                .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer() {
-                    @Override
-                    protected void initChannel(Channel ch) {
-                        ch.pipeline().addLast(new StringEncoder(StandardCharsets.UTF_8));
-                        ch.pipeline().addLast(firstDecorator);
-                        ch.pipeline().addLast(lastDecorator);
-                        ch.pipeline().addLast(new StringDecoder(StandardCharsets.UTF_8));
-                        ch.pipeline().addLast(new ClientHandler());
-                        ch.pipeline().addLast(new ByteArrayEncoder());
-                        ch.pipeline().addLast(new ChunkedWriteHandler());
-                    }
-                });
+        try {
+            /**
+             * 确定消息格式(byte/string)、是否使用SSL的组合方式
+             * 1 byte & SSL
+             * 2 byte & No SSL
+             * 3 string & SSL
+             * 4 string & No SSL
+             */
+            int type = 3;
+            SslContext sslCtx = null;
+            NettyServer.MessageFormat messageFormat;
+            if (type < 3) {
+                messageFormat = NettyServer.MessageFormat.BYTES;
+            } else {
+                messageFormat = NettyServer.MessageFormat.STRING;
+            }
+            if ((type % 2) == 1) {
+                sslCtx = SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+            }
 
-        Channel channel = bootstrap.connect("localhost", 8080).channel();
-        Random random = new Random();
-        while (true) {
-            String message = UUID.randomUUID().toString();
-            channel.writeAndFlush(message);
-            System.out.println("send " + message);
-            Thread.sleep(random.nextInt(2000));
+            runNettyClient(messageFormat, sslCtx);
+        } catch (Exception ex) {
+
         }
     }
 
-    private static class ClientHandler extends SimpleChannelInboundHandler<String> {
+    private static void runNettyClient(NettyServer.MessageFormat messageFormat, SslContext sslContext) {
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            NioEventLoopGroup group = new NioEventLoopGroup();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer() {
+                        @Override
+                        protected void initChannel(Channel ch) {
+                            if (sslContext != null) {
+                                ch.pipeline().addLast(sslContext.newHandler(ch.alloc(), "192.168.43.196", 1884));
+                            }
+                            if (NettyServer.MessageFormat.STRING == messageFormat) {
+                                ch.pipeline().addLast(new StringEncoder(StandardCharsets.UTF_8));
+                                ch.pipeline().addLast(new MessageDecorator("#"));
+                                ch.pipeline().addLast(new MessageDecorator("*"));
+                                ch.pipeline().addLast(new StringDecoder(StandardCharsets.UTF_8));
+                            }
+                            ch.pipeline().addLast(new ClientHandler());
+                        }
+                    });
+
+            Channel channel = bootstrap.connect("192.168.43.196", 1884).channel();
+            Random random = new Random();
+            while (true) {
+                //将发送的信息编码成字节数组发送给服务端，isActive必需(SSL握手可能耗时比较长，会执行一到两次sleep)
+                if (channel.isActive() && channel.isOpen()) {
+                    String message = UUID.randomUUID().toString();
+                    if (NettyServer.MessageFormat.STRING == messageFormat) {
+                        channel.writeAndFlush(message);
+                    } else if (NettyServer.MessageFormat.BYTES == messageFormat) {
+                        channel.writeAndFlush(Unpooled.wrappedBuffer(message.getBytes(StandardCharsets.UTF_8)));
+                    }
+                    System.out.println("send " + message);
+                }
+                int sleepInterval = random.nextInt(200);
+                Thread.sleep(sleepInterval);
+            }
+        } catch (Exception ex) {
+
+        }
+    }
+
+    private static class ClientHandler extends ChannelInboundHandlerAdapter {
         @Override
         public void channelActive(ChannelHandlerContext ctx) throws Exception {
-            String sendInfo = "Hello 这里是客户端  你好啊！";
-            ctx.writeAndFlush(sendInfo); // 必须有flush
+            System.out.println("已连接到:" + ctx.channel().remoteAddress());
         }
 
         @Override
-        protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-            System.out.println("客户端收到:" + msg);
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            if (msg instanceof String) {
+                System.out.println("客户端收到:" + msg);
+            } else {
+                ByteBuf byteBuf = (ByteBuf) msg;
+                int readLength = byteBuf.readableBytes();
+                byte[] bytes = new byte[readLength];
+                byteBuf.readBytes(bytes);
+                String receiveMessage = new String(bytes);
+                System.out.println("客户端收到:" + receiveMessage);
+            }
         }
     }
 
     static class MessageDecorator extends ChannelOutboundHandlerAdapter {
         private String decorateChar;
 
-        private void setDecorateChar(String decorateChar) {
+        public MessageDecorator(String decorateChar) {
             this.decorateChar = decorateChar;
         }
 
--- web/src/main/java/cn/gigahome/web/netty/examples/NettyServer.java
+++ web/src/main/java/cn/gigahome/web/netty/examples/NettyServer.java
@@ -7,6 +7,11 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +20,11 @@ import java.nio.charset.StandardCharsets;
 public class NettyServer implements Runnable {
     private int port;
 
+    public enum MessageFormat {
+        BYTES,
+        STRING
+    }
+
     public NettyServer(int port) {
         this.port = port;
     }
@@ -22,13 +32,33 @@ public class NettyServer implements Runnable {
     @Override
     public void run() {
         try {
-            runNettyServer();
+            /**
+             * 确定消息格式(byte/string)、是否使用SSL的组合方式
+             * 1 byte & SSL
+             * 2 byte & No SSL
+             * 3 string & SSL
+             * 4 string & No SSL
+             */
+            int type = 1;
+            SslContext sslCtx = null;
+            MessageFormat messageFormat;
+            if (type < 3) {
+                messageFormat = MessageFormat.BYTES;
+            } else {
+                messageFormat = MessageFormat.STRING;
+            }
+            if ((type % 2) == 1) {
+                SelfSignedCertificate ssc = new SelfSignedCertificate();
+                sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
+            }
+
+            runNettyServer(messageFormat, sslCtx);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    private void runNettyServer() throws Exception {
+    private void runNettyServer(MessageFormat messageFormat, SslContext sslContext) throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
@@ -38,15 +68,18 @@ public class NettyServer implements Runnable {
                     .handler(new SimpleServerHandler())
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
+                        protected void initChannel(SocketChannel ch) throws Exception {
                             //在这里加入处理读写的handler
                             ChannelPipeline pipeline = ch.pipeline();
-//                            pipeline.addLast(new StringDecoder());
-//                            pipeline.addLast(new StringEncoder());
-//                            pipeline.addLast(new StringEncoder(StandardCharsets.UTF_8));
-//                            pipeline.addLast(new StringDecoder(StandardCharsets.UTF_8));
+                            if (sslContext != null) {
+                                // SSL自签名证书
+                                pipeline.addLast(sslContext.newHandler(ch.alloc()));
+                            }
+                            if (messageFormat == MessageFormat.STRING) {
+                                pipeline.addLast(new StringEncoder(StandardCharsets.UTF_8));
+                                pipeline.addLast(new StringDecoder(StandardCharsets.UTF_8));
+                            }
                             pipeline.addLast(new ServerHandler());
-//                            pipeline.addLast(new ByteArrayDecoder());
                         }
                     });
             ChannelFuture f = b.bind(this.port).sync();
@@ -57,8 +90,7 @@ public class NettyServer implements Runnable {
         }
     }
 
-
-    private static class ServerHandler extends ChannelInboundHandlerAdapter {
+    private class ServerHandler extends ChannelInboundHandlerAdapter {
         /**
          * 如果线程堵塞，会一次收到客户端多次发送的信息
          * <p>
@@ -79,24 +111,29 @@ public class NettyServer implements Runnable {
 
         @Override
         public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-            // 接收到字节数组后,解码成字符串，返回编码成字节数组的确认信息
-            if (lastBuf == null) {
-                lastBuf = (ByteBuf) msg;
-            } else {
-                lastBuf = Unpooled.wrappedBuffer(lastBuf, (ByteBuf) msg);
-            }
-            // 每次固定读取一个消息长度的字节，剩余未读取的字节留到下次接收到客户端传输时读取
-            byte[] bytes = new byte[fixMessageLength];
-            while (lastBuf.readableBytes() > fixMessageLength) {
-                lastBuf.readBytes(bytes);
-                String receiveMessage = new String(bytes);
-                logger.info("服务端收到: " + receiveMessage);
-                ctx.writeAndFlush(Unpooled.wrappedBuffer(receiveMessage.getBytes(StandardCharsets.UTF_8)));
+            if (msg instanceof ByteBuf) {
+                // 接收到字节数组后,解码成字符串，返回编码成字节数组的确认信息
+                if (lastBuf == null) {
+                    lastBuf = (ByteBuf) msg;
+                } else {
+                    lastBuf = Unpooled.wrappedBuffer(lastBuf, (ByteBuf) msg);
+                }
+                // 每次<b>固定读取</b>一个消息长度的字节，剩余未读取的字节留到下次接收到客户端传输时读取
+                byte[] bytes = new byte[fixMessageLength];
+                while (lastBuf.readableBytes() > fixMessageLength) {
+                    lastBuf.readBytes(bytes);
+                    String receiveMessage = new String(bytes);
+                    logger.info("服务端收到: " + receiveMessage);
+                    ctx.writeAndFlush(Unpooled.wrappedBuffer(receiveMessage.getBytes(StandardCharsets.UTF_8)));
+                }
+            } else if (msg instanceof String) {
+                logger.info("服务端收到: " + msg);
+                ctx.writeAndFlush(msg);
             }
         }
     }
 
-    private static class SimpleServerHandler extends ChannelInboundHandlerAdapter {
+    private class SimpleServerHandler extends ChannelInboundHandlerAdapter {
         @Override
         public void channelActive(ChannelHandlerContext ctx) throws Exception {
             System.out.println("server channel Active");
