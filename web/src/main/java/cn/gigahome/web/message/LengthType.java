7bd5dd3a602dbd5aebfd125046e5a52374effb37
null <aibw@kaisagroup.com>
2020-07-06 17:23:48 +0800

    NIO read & write

---
+++ web/src/main/java/cn/gigahome/web/message/LengthType.java
@@ -0,0 +1,16 @@
+package cn.gigahome.web.message;
+
+public enum LengthType {
+    /**
+     * 无符号整型，最大值511
+     */
+    UNSIGNED_BYTE,
+    /**
+     * 双字节整型,最大值262143,MSB LSB编码
+     */
+    DOUBLE_BYTE,
+    /**
+     * 变长字节整型,前一字节的最高位为1时，下一字节也包含在内，最多4个字节
+     */
+    VARIANT_BYTE
+}
---
+++ web/src/main/java/cn/gigahome/web/message/PropertyIdentifier.java
@@ -0,0 +1,33 @@
+package cn.gigahome.web.message;
+
+public enum PropertyIdentifier {
+    NULL(null, null),
+    VERSION(DataType.UNSIGNED_BYTE, null),
+    TIMESTAMP(DataType.LONG, null),
+    METHOD_NAME(DataType.STRING, LengthType.UNSIGNED_BYTE);
+
+    private DataType dateType;
+
+    private LengthType lengthType;
+
+    PropertyIdentifier(DataType dataType, LengthType lengthType) {
+        this.dateType = dataType;
+        this.lengthType = lengthType;
+    }
+
+    public DataType getDateType() {
+        return dateType;
+    }
+
+    public void setDateType(DataType dateType) {
+        this.dateType = dateType;
+    }
+
+    public LengthType getLengthType() {
+        return lengthType;
+    }
+
+    public void setLengthType(LengthType lengthType) {
+        this.lengthType = lengthType;
+    }
+}
